63ad893b0d26352edc51a581686c3740e13a5e3f
luriloring <cjsstkaos@naver.com>
2016-02-27 13:46:20 +0900

    잡초양 좌표 고정

--- game.js
+++ game.js
@@ -12,13 +12,13 @@ var debut = [
   new Piece(8300, 50),
   new Piece(10450, 450),
 
-  new Weed(800 + Math.random()*600,300),
+  new Weed(1400,300),
   new Mouse(400 + Math.random()*600,300,10+Math.random()*10),
-  new Weed(1500 + Math.random()*1600,300),
+  new Weed(2300,300),
   new Beetle(4400 + Math.random()*50, 400),
-  new Weed(7000 + Math.random()*500,100),
+  new Weed(7200,100),
   new Bat(7300 + Math.random()*600,650+Math.random()*50),
-  new Weed(9000 + Math.random()*600,50),
+  new Weed(9300,50),
   new Mouse(9300 + Math.random()*400,300,5+Math.random()*10)
 ];
 
@@ -28,19 +28,19 @@ var regular = [
   new Mouse(4100 + Math.random()*100,400,10+Math.random()*10),
   new Beetle(5200 + Math.random()*500,200),
   new Mouse(7100 + Math.random()*400,100,10+Math.random()*10),
-  new Weed(7500 + Math.random()*400,600),
+  new Weed(7700,600),
   new Beetle(9000 + Math.random()*600,50),
-  new Weed(9400 + Math.random()*300,300)
+  new Weed(9500,300)
 ];
 
 // Pro
 var pro = [
-  new Weed(400 + Math.random()*300,300),
+  new Weed(600,300),
   new Mouse(1000 + Math.random()*600,300,10+Math.random()*10),
   new Beetle(1500 + Math.random()*500,200),
   new Bat(3300 + Math.random()*200,450+Math.random()*50),
-  new Weed(5200 + Math.random()*600,200),
-  new Weed(7300 + Math.random()*300,600),
+  new Weed(5400,200),
+  new Weed(7500,600),
   new Weed(8400,400),
   new Mouse(9100 + Math.random()*400,100,10+Math.random()*10),
   new Mouse(9500 + Math.random()*200,300,5+Math.random()*10)
@@ -48,37 +48,37 @@ var pro = [
 
 // Master
 var master = [
-  new Mouse(700 + Math.random()*600,300,10+Math.random()*10),
+  new Mouse(1000,300,10+Math.random()*10),
   new Weed(2000 + Math.random()*500,300),
   new Mouse(2100 + Math.random()*600,300,10+Math.random()*10),
   new Mouse(2400 + Math.random()*600,300,10+Math.random()*10),
   new Beetle(2800 + Math.random()*700,300),
-  new Weed(3800 + Math.random()*300,300),
+  new Weed(4200,300),
   new Bat(4300 + Math.random()*100,350+Math.random()*10),
-  new Weed(5100 + Math.random()*200,200),
+  new Weed(5200,200),
   new Mouse(5100 + Math.random()*400,100,10+Math.random()*10),
-  new Weed(6000 + Math.random()*800,200),
-  new Weed(7500 + Math.random()*300,100),
+  new Weed(6400,200),
+  new Weed(7700,100),
   new Beetle(7400 + Math.random()*500,600),
   new Weed(8400,200),
   new Mouse(9100 + Math.random()*400,100,10+Math.random()*10),
   new Beetle(9400 + Math.random()*300,200),
-  new Weed(9600 + Math.random()*200,300)
+  new Weed(9700,300)
 ];
 
 // Master +
 var master2 = [
-  new Weed(200 + Math.random()*200,300),
+  new Weed(300,300),
   new Mouse(300 + Math.random()*600,300,15+Math.random()*10),
-  new Weed(2000 + Math.random()*900,300),
+  new Weed(2500,300),
   new Beetle(1500 + Math.random()*500,600),
   new Bat(2200 + Math.random()*800,400+Math.random()*50),
   new Beetle(2500 + Math.random()*800,600),
   new Mouse(3200 + Math.random()*600,300,10+Math.random()*10),
-  new Weed(3600 + Math.random()*100,300),
+  new Weed(3650,300),
   new Beetle(4400 + Math.random()*200,600),
-  new Weed(4900 + Math.random()*100,200),
-  new Weed(5300 + Math.random()*800,200),
+  new Weed(4950,200),
+  new Weed(5700,200),
   new Mouse(5400 + Math.random()*400,200,10+Math.random()*10),
   new Beetle(5500 + Math.random()*300,300),
   new Beetle(6200 + Math.random()*500,600),
@@ -89,11 +89,11 @@ var master2 = [
   new Weed(8400,600),
   new Bat(8500 + Math.random()*100,60+Math.random()*30),
   new Mouse(9100 + Math.random()*600,100,10+Math.random()*10),
-  new Weed(8700 + Math.random()*300,100),
+  new Weed(8900,100),
   new Beetle(9200 + Math.random()*300,100),
-  new Weed(9400 + Math.random()*300,200),
+  new Weed(9600,200),
   new Beetle(9600 + Math.random()*300,400),
-  new Weed(10200 + Math.random()*200,200)
+  new Weed(10300,200)
 ];
 
 
@@ -116,7 +116,10 @@ var gameStart = function(lv){
     my.gravity = true;
     my.tag = 'player';
     my.moveSpeed = 20;
-    my.havepiece = 0;
+    
+    my.start(function(){
+        my.havepiece = 0;
+    });
 
     my.animate(resource.player, 133, 191, {
       left: 2,
