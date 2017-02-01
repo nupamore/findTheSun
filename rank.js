6c48059c5727ffd548e8b99b33e8330b746f4e14
nupamore <nupamore@google.com>
2017-02-02 00:26:04 +0900

    create iframe

--- rank.js
+++ rank.js
@@ -8,6 +8,8 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+app.use('/', express.static('./'));
+
 
 var rank = [
   { player: '실버제로', level:5, time: 215120 },
